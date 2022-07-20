958da6239ff74d04ed4b93a06a7b3004b4d81c38
BENJY33X <A.STAILI@student.youcode.ma>
2022-07-20 15:54:47 +0100

    finished the parti front end the page home

--- pages/home.tsx
+++ pages/home.tsx
@@ -28,45 +28,45 @@ const home = ({ providers }: any) => {
           </div>
 
           {/* {Object.values(providers).map((provider:any) => (
-                <div key={provider.name}>
-                  <div className="pl-4">
-                    <button
-                      className="text-blue-700 font-semibold rounded-full border border-blue-700 px-5 py-1.5 transition-all hover:border-2"
-                      onClick={() => signIn(provider.id, { callbackUrl: "/" })}
-                    >
-                      Sign in
-                    </button>
-                  </div>
-                </div>
+                <div key={provider.name}> */}
+          <div className="pl-4">
+            <button
+              className="text-blue-700 font-semibold rounded-full border border-blue-700 px-5 py-1.5 transition-all hover:border-2"
+              // onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+            >
+              Sign in
+            </button>
+          </div>
+          {/* </div>
               ))} */}
         </div>
       </header>
 
-      {/* <main className="flex flex-col xl:flex-row items-center max-w-screen-lg mx-auto">
-            <div className="space-y-6 xl:space-y-10">
-              <h1 className="text-3xl md:text-5xl text-amber-800/80 max-w-xl !leading-snug pl-4 xl:pl-0">
-                Welcome to your professional community
-              </h1>
-              <div className="space-y-4">
-                <div className="intent">
-                  <h2 className="text-xl">Search for a job</h2>
-                  <ArrowForwardIosRoundedIcon className="text-gray-700" />
-                </div>
-                <div className="intent">
-                  <h2 className="text-xl">Find a person you know</h2>
-                  <ArrowForwardIosRoundedIcon className="text-gray-700" />
-                </div>
-                <div className="intent">
-                  <h2 className="text-xl">Learn a new skill</h2>
-                  <ArrowForwardIosRoundedIcon className="text-gray-700" />
-                </div>
-              </div>
+      <main className="flex flex-col xl:flex-row items-center justify-center gap-7 ">
+        <div className="space-y-6 xl:space-y-10 flex flex-col justify-center">
+          <h1 className="text-3xl md:text-5xl text-amber-800/80 max-w-xl !leading-snug pl-4 xl:pl-0">
+            Welcome to your professional community
+          </h1>
+          <div className="space-y-4">
+            <div className="intent">
+              <h2 className="text-xl">Search for a job</h2>
+              <ArrowForwardIosRoundedIcon className="text-gray-700" />
             </div>
-    
-            <div className="relative xl:absolute w-80 h-80 xl:w-[650px] xl:h-[650px] top-14 right-5">
-              <Image src="https://rb.gy/vkzpzt" layout="fill" priority />
+            <div className="intent">
+              <h2 className="text-xl">Find a person you know</h2>
+              <ArrowForwardIosRoundedIcon className="text-gray-700" />
             </div>
-          </main> */}
+            <div className="intent">
+              <h2 className="text-xl">Learn a new skill</h2>
+              <ArrowForwardIosRoundedIcon className="text-gray-700" />
+            </div>
+          </div>
+        </div>
+
+        <div className="relative w-80 h-80 xl:w-[650px] xl:h-[650px] ">
+          <img src="https://rb.gy/vkzpzt" />
+        </div>
+      </main>
     </div>
   );
 };
