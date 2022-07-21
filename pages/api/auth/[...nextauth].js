1577952a53863a756b90e89e8fb49e44388b48dc
BENJY33X <A.STAILI@student.youcode.ma>
2022-07-21 17:04:05 +0100

    first collabe

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,20 @@
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import clientPromise from "../../../lib/mongodb";
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.JWT_SECRET,
+  adapter: MongoDBAdapter(clientPromise),
+  pages: {
+    signIn: "/home",
+  },
+  session: {
+    strategy: "jwt",
+  },
+});
