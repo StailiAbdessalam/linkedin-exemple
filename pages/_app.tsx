a39857a686a84f3fcae087122e27cc522775d4a2
BENJY33X <A.STAILI@student.youcode.ma>
2022-07-22 13:47:05 +0100

    finich push today resoul all bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "next-themes";
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,51 @@
+import Head from "next/head";
+import { signOut } from "next-auth/react";
+import { getSession, useSession } from "next-auth/react";
+import { Header, Sidebar } from "../components";
+import Feed from "../components/Feed";
+
+const Home = () => {
+  return (
+    <div className="bg-[#F3F2EF] dark:bg-black dark:text-white h-screen overflow-y-scroll md:space-y-6">
+      <Head>
+        <title>Feed | LinkedIn</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+
+      <main className="flex justify-center gap-x-5 px-4 sm:px-12">
+        <div className="flex flex-col md:flex-row gap-5">
+          <Sidebar />
+          <Feed posts={posts} />
+        </div>
+        {/* <Widgets articles={articles} />
+        <AnimatePresence>
+          {modalOpen && (
+            <Modal handleClose={() => setModalOpen(false)} type={modalType} />
+          )}
+        </AnimatePresence> */}
+      </main>
+    </div>
+  );
+};
+
+export default Home;
+
+export async function getServerSideProps(context) {
+  // Check if the user is authenticated on the server...
+  const session = await getSession(context);
+  if (!session) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/home",
+      },
+    };
+  } else {
+    return {
+      props: {
+        session,
+      },
+    };
+  }
+}
