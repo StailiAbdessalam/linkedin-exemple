f03b63c252bd9b113cb76fc1c02c0ec61cf2db9c
BENJY33X <abdoycode@gmail.com>
2022-07-26 13:58:37 +0100

    Feed de pages post

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,29 +1,29 @@
-import { useEffect, useState } from "react";
-import { useRecoilState } from "recoil";
+// import { useEffect, useState } from "react";
+// import { useRecoilState } from "recoil";
 import Input from "./Input";
-import { handlePostState, useSSRPostsState } from "../atoms/postAtom";
-import Post from "./Post";
+// import { handlePostState, useSSRPostsState } from "../atoms/postAtom";
+// import Post from "./Post";
 
-function Feed({ posts }: any) {
-  const [realtimePosts, setRealtimePosts] = useState([]);
-  const [handlePost, setHandlePost] = useRecoilState(handlePostState);
-  const [useSSRPosts, setUseSSRPosts] = useRecoilState(useSSRPostsState);
+function Feed() {
+  // const [realtimePosts, setRealtimePosts] = useState([]);
+  // const [handlePost, setHandlePost] = useRecoilState(handlePostState);
+  // const [useSSRPosts, setUseSSRPosts] = useRecoilState(useSSRPostsState);
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await fetch("/api/posts", {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-      });
+  // useEffect(() => {
+  //   const fetchPosts = async () => {
+  //     const response = await fetch("/api/posts", {
+  //       method: "GET",
+  //       headers: { "Content-Type": "application/json" },
+  //     });
 
-      const responseData = await response.json();
-      setRealtimePosts(responseData);
-      setHandlePost(false);
-      setUseSSRPosts(false);
-    };
+  //     const responseData = await response.json();
+  //     setRealtimePosts(responseData);
+  //     setHandlePost(false);
+  //     setUseSSRPosts(false);
+  //   };
 
-    fetchPosts();
-  }, [handlePost]);
+  //   fetchPosts();
+  // }, [handlePost]);
 
   return (
     <div className="space-y-6 pb-24 max-w-lg">
