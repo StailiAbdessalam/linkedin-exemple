5ca4e0f1d17725d34f23507d035723eb0029c4f9
BENJY33X <abdoycode@gmail.com>
2022-08-02 11:16:15 +0100

    input components

--- components/Input.tsx
+++ components/Input.tsx
@@ -5,29 +5,29 @@ import PhotoSizeSelectActualIcon from "@mui/icons-material/PhotoSizeSelectActual
 import VideoCameraBackIcon from "@mui/icons-material/VideoCameraBack";
 import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
 import ArticleIcon from "@mui/icons-material/Article";
-// import { useRecoilState } from "recoil";
+import { useRecoilState } from "recoil";
 import { modalState, modalTypeState } from "../atoms/modalAtom";
 
 function Input() {
-  const { data: session } = useSession();
-  // const [modalOpen, setModalOpen] = useRecoilState(modalState);
-  // const [modalType, setModalType] = useRecoilState(modalTypeState);
+  const { data: session }:any = useSession();
+  const [modalOpen, setModalOpen] = useRecoilState(modalState);
+  const [modalType, setModalType] = useRecoilState(modalTypeState);
 
   return (
     <div className="bg-white dark:bg-[#1D2226] rounded-lg p-3 space-y-3 border border-gray-300 dark:border-none">
       <div className="flex items-center space-x-2">
-        {/* <Avatar
+        <Avatar
           src={session?.user?.image}
           className="!h-10 !w-10 cursor-pointer"
-        /> */}
+        />
         <motion.button
           whileHover={{ scale: 1.01 }}
           whileTap={{ scale: 0.99 }}
           className="rounded-full border border-gray-400 py-2.5 px-3 opacity-80 hover:opacity-100 font-medium w-full text-left"
-          // onClick={() => {
-          //   setModalOpen(true);
-          //   setModalType("dropIn");
-          // }}
+          onClick={() => {
+            setModalOpen(true);
+            setModalType("dropIn");
+          }}
         >
           Start a post
         </motion.button>
