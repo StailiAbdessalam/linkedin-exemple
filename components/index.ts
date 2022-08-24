ca6d6dfbc33cff315c29c261616ca02e782a3458
BENJY33X <abdoycode@gmail.com>
2022-08-24 17:13:23 +0100

    export components widgets

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,5 @@
 export { default as HeaderLink } from "./HeaderLink";
 export { default as Header } from "./Header";
 export { default as Sidebar } from "./sidebar";
+export { default as Widgets } from "./Widgets";
+
