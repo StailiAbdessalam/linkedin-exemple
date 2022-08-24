f28bdd4a914b93a2ad09143d1b6e523e0c65a72a
BENJY33X <abdoycode@gmail.com>
2022-08-24 16:35:35 +0100

    add the part of feed to view all post

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,37 +1,37 @@
-// import { useEffect, useState } from "react";
-// import { useRecoilState } from "recoil";
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
 import Input from "./Input";
-// import { handlePostState, useSSRPostsState } from "../atoms/postAtom";
-// import Post from "./Post";
+import { handlePostState, useSSRPostsState } from "../atoms/postAtom";
+import Post from "./Post";
 
-function Feed() {
-  // const [realtimePosts, setRealtimePosts] = useState([]);
-  // const [handlePost, setHandlePost] = useRecoilState(handlePostState);
-  // const [useSSRPosts, setUseSSRPosts] = useRecoilState(useSSRPostsState);
+function Feed({posts}:any) {
+  const [realtimePosts, setRealtimePosts] = useState([]);
+  const [handlePost, setHandlePost] = useRecoilState(handlePostState);
+  const [useSSRPosts, setUseSSRPosts] = useRecoilState(useSSRPostsState);
 
-  // useEffect(() => {
-  //   const fetchPosts = async () => {
-  //     const response = await fetch("/api/posts", {
-  //       method: "GET",
-  //       headers: { "Content-Type": "application/json" },
-  //     });
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/posts", {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      });
 
-  //     const responseData = await response.json();
-  //     setRealtimePosts(responseData);
-  //     setHandlePost(false);
-  //     setUseSSRPosts(false);
-  //   };
+      const responseData = await response.json();
+      setRealtimePosts(responseData);
+      setHandlePost(false);
+      setUseSSRPosts(false);
+    };
 
-  //   fetchPosts();
-  // }, [handlePost]);
+    fetchPosts();
+  }, [handlePost]);
 
   return (
     <div className="space-y-6 pb-24 max-w-lg">
       <Input />
       {/* Posts */}
-      {/* {!useSSRPosts
-        ? realtimePosts.map((post) => <Post key={post._id} post={post} />)
-        : posts.map((post) => <Post key={post._id} post={post} />)} */}
+      {!useSSRPosts
+        ? realtimePosts.map((post:any) => <Post key={post._id} post={post} />)
+        : posts?.map((post:any) => <Post key={post._id} post={post} />)}
     </div>
   );
 }
